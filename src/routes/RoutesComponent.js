83c211335bed1afe4ad14f1086d7bd8c1b02ffbe
vinicius ribeiro <viniciusrad@yahoo.com.br>
2024-10-05 17:52:49 -0300

    wip - componente de cadastro de usuario inserido

--- src/routes/RoutesComponent.js
+++ src/routes/RoutesComponent.js
@@ -4,6 +4,7 @@ import { Login } from '../pages/login/Login.js'; // Importar o componente Login
 import { Home } from '../pages/home';
 import  FormularioVeiculo  from '../pages/veiculo/cadastro-veiculos.js';
 import CadastroProprietario from '../pages/proprietario/proprietario_carro.js';
+import CadastroUsuario from '../pages/usuario/cadastro-usuario.js';
 
 
 const AppRoutes = () => {
@@ -14,6 +15,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/veiculo/cadastro" element={<FormularioVeiculo />} />
         <Route path="/proprietario/carro" element={<CadastroProprietario />} />
+        <Route path="/usuario/cadastro" element={<CadastroUsuario />} />
         {/* Outras rotas */}
       </Routes>
     </Router>
