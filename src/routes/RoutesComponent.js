b0f78115b86926bf0e0ceb235891289ba9b11232
vinicius ribeiro <viniciusrad@yahoo.com.br>
2024-10-19 22:46:51 -0300

    fix - inclussão de rotas para as novas paginas

--- src/routes/RoutesComponent.js
+++ src/routes/RoutesComponent.js
@@ -12,6 +12,8 @@ import ListarVeiculos from '../pages/veiculo/listar-veiculo.js';
 import HistoricoAgendamentos from '../pages/vagas/historico-agendamentos.js';
 import CadastroCartao from '../pages/cartao/cadastro-cartao.js';
 import MeuHistorico from '../pages/historico/meu-historico.js';
+import MeusAgendamentos from '../pages/agendamentos/meus-agendamentos.js';
+import ApagarConta from '../pages/usuario/apagar-conta.js';
 const AppRoutesWithFooter = () => {
   const navigate = useNavigate();
 
@@ -24,11 +26,13 @@ const AppRoutesWithFooter = () => {
         <Route path="/veiculo/lista" element={<ListarVeiculos />} />
         <Route path="/proprietario/carro" element={<CadastroProprietario />} />
         <Route path="/usuario/cadastro" element={<CadastroUsuario />} />
+        <Route path="/usuario/apagar-conta" element={<ApagarConta />} />
         <Route path="/vagas/reservar" element={<ReservaVaga />} />
         <Route path="/vagas/listar" element={<ListaVagas />} />
         <Route path="/vagas/historico" element={<HistoricoAgendamentos />} />
         <Route path="/cartao/cadastro" element={<CadastroCartao />} />
         <Route path="/historico/meu-historico" element={<MeuHistorico />} />
+        <Route path="/agendamentos/meus-agendamentos" element={<MeusAgendamentos />} />
       </Routes>
       <Footer navigate={navigate} />
     </>
