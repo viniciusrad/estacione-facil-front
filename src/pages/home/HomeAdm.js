6c0c3d19845471ea97250730a6bcff9bedfe4d56
vinicius ribeiro <viniciusrad@yahoo.com.br>
2024-10-19 23:13:16 -0300

    feat - separação da home para o adm

---
+++ src/pages/home/HomeAdm.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext } from 'react';
+import MessagePopUp from '../../components/MessagePopUp';
+import { LogoDiv } from '../../components/LogoDiv';
+
+import { UsuarioContext } from '../../context/UsuarioContext';
+
+function HomeAdm({ handleNavigation }) {
+
+    const { tipoUsuario } = useContext(UsuarioContext);
+
+
+    return (
+        <>
+            <div>
+                <LogoDiv text='Home' />
+                <div className='home-body'>
+                    <h2>Tipo de Usuário: {tipoUsuario}</h2>
+                    <div className="container-home">
+                        <button className="item-menu-home" onClick={() => handleNavigation('/veiculo/cadastro')}>
+                            <img src="/img/carro.png" alt="" />
+                            <h4>Cadastrar Veículo</h4>
+                        </button>
+                        <button className="item-menu-home" onClick={() => handleNavigation('/vagas/reservar')}>
+                            <img src="/img/reserva_1.png" alt="" />
+                            <h4>Reservar Vaga</h4>
+                        </button>
+                        <button className="item-menu-home" onClick={() => handleNavigation('/pagamento')}>
+                            <img src="/img/pagamento-seguro.png" alt="" />
+                            <h4>Pagamento</h4>
+                        </button>
+                    </div>
+                    <div className="container-home">
+                        <button className="item-menu-home" onClick={() => handleNavigation('/veiculo/lista')}>
+                            <img src="/img/engarrafamento.png" alt="" />
+                            <h4>Escolher Veículo</h4>
+                        </button>
+                        <button className="item-menu-home" onClick={() => handleNavigation('/agendamentos')}>
+                            <img src="/img/reserva_2.png" alt="" />
+                            <h4>Agendamentos</h4>
+                        </button>
+                    </div>
+                    <div className="container-home">
+                        <button className="item-menu-home" onClick={() => handleNavigation('/vagas/historico')}>
+                            <img src="/img/historico.png" alt="" />
+                            <h4>Histórico</h4>
+                        </button>
+                        <button className="item-menu-home" onClick={() => handleNavigation('/usuario/apagar')}>
+                            <img src="/img/deletar-usuario.png" alt="" />
+                            <h4>Apagar Conta</h4>
+                        </button>
+                        <button className="item-menu-home" onClick={() => handleNavigation('/usuario/atualizar')}>
+                            <img src="/img/processamento-de-dados.png" alt="" />
+                            <h4>Atualizar Dados</h4>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default HomeAdm;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,10 @@
 import './index.css';
 import { LogoDiv } from '../../components/LogoDiv';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import HomeAdm from './HomeAdm';
+import { UsuarioContext } from '../../context/UsuarioContext';
+
 
 export const Home = () => {
     const navigate = useNavigate();
@@ -9,49 +13,12 @@ export const Home = () => {
         navigate(path);
     };
 
+    const { tipoUsuario } = useContext(UsuarioContext);
+
     return (
-        <div>
-            <LogoDiv text='Home' />
-            <div className='home-body'>
-                <div className="container-home">
-                    <button className="item-menu-home" onClick={() => handleNavigation('/veiculo/cadastro')}>
-                        <img src="/img/carro.png" alt="" />
-                        <h4>Cadastrar Veículo</h4>
-                    </button>
-                    <button className="item-menu-home" onClick={() => handleNavigation('/vagas/reservar')}>
-                        <img src="/img/reserva_1.png" alt="" />
-                        <h4>Reservar Vaga</h4>
-                    </button>
-                    <button className="item-menu-home" onClick={() => handleNavigation('/pagamento')}>
-                        <img src="/img/pagamento-seguro.png" alt="" />
-                        <h4>Pagamento</h4>
-                    </button>
-                </div>
-                <div className="container-home">
-                    <button className="item-menu-home" onClick={() => handleNavigation('/veiculo/lista')}>
-                        <img src="/img/engarrafamento.png" alt="" />
-                        <h4>Escolher Veículo</h4>
-                    </button>
-                    <button className="item-menu-home" onClick={() => handleNavigation('/agendamentos')}>
-                        <img src="/img/reserva_2.png" alt="" />
-                        <h4>Agendamentos</h4>
-                    </button>
-                </div>
-                <div className="container-home">
-                    <button className="item-menu-home" onClick={() => handleNavigation('/vagas/historico')}>
-                        <img src="/img/historico.png" alt="" />
-                        <h4>Histórico</h4>
-                    </button>
-                    <button className="item-menu-home" onClick={() => handleNavigation('/usuario/apagar')}>
-                        <img src="/img/deletar-usuario.png" alt="" />
-                        <h4>Apagar Conta</h4>
-                    </button>
-                    <button className="item-menu-home" onClick={() => handleNavigation('/usuario/atualizar')}>
-                        <img src="/img/processamento-de-dados.png" alt="" />
-                        <h4>Atualizar Dados</h4>
-                    </button>
-                </div>
-            </div>
-        </div>
+        <>
+            {tipoUsuario == 'administrador' ? <HomeAdm handleNavigation={handleNavigation} /> : <h1>test</h1>}
+        </>
+
     )
 }
